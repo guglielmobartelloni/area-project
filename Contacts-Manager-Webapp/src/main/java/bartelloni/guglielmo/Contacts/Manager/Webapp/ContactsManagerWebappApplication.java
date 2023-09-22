5f62f0e4d5d2c7c6bb6dde07cfff5456effe7f5b
samurai <samurai@MacBook-Air-di-samurai.local>
2023-09-22 18:40:22 +0200

    Added web project

---
+++ Contacts-Manager-Webapp/src/main/java/bartelloni/guglielmo/Contacts/Manager/Webapp/ContactsManagerWebappApplication.java
@@ -0,0 +1,13 @@
+package bartelloni.guglielmo.Contacts.Manager.Webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsManagerWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsManagerWebappApplication.class, args);
+	}
+
+}
---
+++ Contacts-Manager-Webapp/src/test/java/bartelloni/guglielmo/Contacts/Manager/Webapp/ContactsManagerWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package bartelloni.guglielmo.Contacts.Manager.Webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsManagerWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

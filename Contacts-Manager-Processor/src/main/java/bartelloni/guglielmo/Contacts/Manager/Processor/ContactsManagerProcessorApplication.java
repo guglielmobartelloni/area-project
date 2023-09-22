e709b6e2fcff7e84d5f58fdfd81724a055ea6eb4
samurai <samurai@MacBook-Air-di-samurai.local>
2023-09-22 18:37:51 +0200

    Initial commit

---
+++ Contacts-Manager-Processor/src/main/java/bartelloni/guglielmo/Contacts/Manager/Processor/ContactsManagerProcessorApplication.java
@@ -0,0 +1,13 @@
+package bartelloni.guglielmo.Contacts.Manager.Processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsManagerProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsManagerProcessorApplication.class, args);
+	}
+
+}
---
+++ Contacts-Manager-Processor/src/test/java/bartelloni/guglielmo/Contacts/Manager/Processor/ContactsManagerProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package bartelloni.guglielmo.Contacts.Manager.Processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsManagerProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

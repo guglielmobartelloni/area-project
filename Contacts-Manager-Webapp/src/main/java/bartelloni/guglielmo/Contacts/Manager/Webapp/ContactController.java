77ec427c648bc8fadff75ff2c9685b2cbe3db0c5
samurai <samurai@MacBook-Air-di-samurai.local>
2023-09-24 21:42:10 +0200

    Added main crud operations

--- Contacts-Manager-Webapp/src/main/java/bartelloni/guglielmo/Contacts/Manager/Webapp/ContactController.java
+++ Contacts-Manager-Webapp/src/main/java/bartelloni/guglielmo/Contacts/Manager/Webapp/ContactController.java
@@ -1,14 +1,29 @@
 package bartelloni.guglielmo.Contacts.Manager.Webapp;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import co.elastic.clients.elasticsearch.core.search.ResponseBody;
+import io.github.wimdeblauwe.hsbt.mvc.HxRefresh;
+import io.github.wimdeblauwe.hsbt.mvc.HxRequest;
+import lombok.extern.java.Log;
+
 
 /**
  * MainController
  */
 @Controller
+@Log
 public class ContactController {
 
     @Autowired
@@ -16,9 +31,33 @@ public class ContactController {
 
     @GetMapping("/new")
     public String newContact(Model model){
-        var contacts = service.getAll();
-        model.addAttribute("contacts", contacts);
+        model.addAttribute("contact", new Contact());
         return "contact-form";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editContact(@PathVariable Long id, Model model){
+        Optional<Contact> optionalContact = service.getById(id);
+        var contact = optionalContact.orElseThrow();
+        model.addAttribute("contact", contact);
+        return "contact-form";
+    }
+
+    @PostMapping("/contact")
+    public String processContact(Model model, @ModelAttribute Contact contact){
+        log.info("Contact: " + contact);
+        service.upsert(contact);
+        model.addAttribute("contacts", service.getAll());
+        return "index";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @HxRequest
+    @HxRefresh
+    public ResponseEntity<String> deleteContact(@PathVariable Long id){
+        var contact = service.getById(id).orElseThrow();
+        service.delete(contact);
+        return new ResponseEntity<String>("", HttpStatus.OK);
     }
 
 }
